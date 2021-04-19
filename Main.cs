862a6201f7153188cbbe9d8ec475e4dc800f44b7
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-04-19 15:19:57 +0200

    Merged two if statements

--- Main.cs
+++ Main.cs
@@ -18,47 +18,44 @@ namespace POVChanger
 
         public override void OnUpdate()
         {
-            if (Input.anyKeyDown)
+            if (Input.anyKeyDown && Input.GetKey(KeyCode.LeftControl))
             {
-                if (Input.GetKey(KeyCode.LeftControl))
+                if (Input.GetKeyDown(KeyCode.Alpha5))
                 {
-                    if (Input.GetKeyDown(KeyCode.Alpha5))
-                    {
-                        _myCam.enabled = false;
-                        var ply = QuickMenu.prop_QuickMenu_0.field_Private_Player_0;
+                    _myCam.enabled = false;
+                    var ply = QuickMenu.prop_QuickMenu_0.field_Private_Player_0;
 
-                        if (ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
-                            .GetBoneTransform(HumanBodyBones.Head).FindChild("HmdPivot").GetComponent<Camera>())
-                        {
-                            _playerCam = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
-                                .GetBoneTransform(HumanBodyBones.Head).FindChild("HmdPivot").GetComponent<Camera>();
-                            _playerCam.enabled = true;
-                            _neck = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
-                                .GetBoneTransform(HumanBodyBones.Neck);
-                            _originalScale = _neck.localScale;
-                            _neck.localScale = new Vector3(0, _originalScale.y, _originalScale.z);
-                        }
-                        else
-                        {
-                            _playerCam = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
-                                .GetBoneTransform(HumanBodyBones.Head).FindChild("HmdPivot").gameObject
-                                .AddComponent<Camera>();
-                            _playerCam.fieldOfView = 90;
-                            _playerCam.nearClipPlane = 0.01f;
-                            _playerCam.enabled = true;
-                            _neck = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
-                                .GetBoneTransform(HumanBodyBones.Neck);
-                            _originalScale = _neck.localScale;
-                            _neck.localScale = new Vector3(0, _originalScale.y, _originalScale.z);
-                        }
-                    }
-
-                    if (Input.GetKeyDown(KeyCode.Alpha6))
+                    if (ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
+                        .GetBoneTransform(HumanBodyBones.Head).FindChild("HmdPivot").GetComponent<Camera>())
                     {
-                        _myCam.enabled = true;
-                        _neck.localScale = _originalScale;
-                        _playerCam.enabled = false;
+                        _playerCam = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
+                            .GetBoneTransform(HumanBodyBones.Head).FindChild("HmdPivot").GetComponent<Camera>();
+                        _playerCam.enabled = true;
+                        _neck = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
+                            .GetBoneTransform(HumanBodyBones.Neck);
+                        _originalScale = _neck.localScale;
+                        _neck.localScale = new Vector3(0, _originalScale.y, _originalScale.z);
                     }
+                    else
+                    {
+                        _playerCam = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
+                            .GetBoneTransform(HumanBodyBones.Head).FindChild("HmdPivot").gameObject
+                            .AddComponent<Camera>();
+                        _playerCam.fieldOfView = 90;
+                        _playerCam.nearClipPlane = 0.01f;
+                        _playerCam.enabled = true;
+                        _neck = ply.transform.Find("ForwardDirection/Avatar").GetComponent<Animator>()
+                            .GetBoneTransform(HumanBodyBones.Neck);
+                        _originalScale = _neck.localScale;
+                        _neck.localScale = new Vector3(0, _originalScale.y, _originalScale.z);
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha6))
+                {
+                    _myCam.enabled = true;
+                    _neck.localScale = _originalScale;
+                    _playerCam.enabled = false;
                 }
             }
         }
